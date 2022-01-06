3a424a97c776e9373e0f22a9101ceac1b9d0d84d
Paul Rayer <paulrayer61@gmail.com>
2022-01-06 15:07:59 +0100

    Affichage 40 lignes ok

--- Metier/Interpreteur.java
+++ Metier/Interpreteur.java
@@ -13,6 +13,10 @@ import java.util.Scanner      ;
 import iut.algo.Console;
 import iut.algo.CouleurConsole;
 
+import static org.fusesource.jansi.Ansi.*;
+import static org.fusesource.jansi.Ansi.Color.*;
+import org.fusesource.jansi.AnsiConsole;
+
 
 /**
  * Classe principale liée au modèle de l'architecture MVC
@@ -20,19 +24,20 @@ import iut.algo.CouleurConsole;
  */
 public class Interpreteur
 {
-	private static final String BG_ROUGE = "\u001b[41m";
-	private static final String BG_RESET = "\u001b[40m";
-
 	private Controleur  controleur; // controleur associé
 
 	private List<Donnee> lstDonnee ; // liste des données
 	private List<String> lstContenu; // contenu du fichier
-	
+	public  static List<String> lstValeursLues;
+	private List<Integer> lstBk;
+
 	private String traceDexecution ; // trace d'éxécution du code
 
 	private String nomFichier; // nom du fichier à lire
 	private int    numeroLigne; // Numéro de la ligne en cours
 	private int    move;
+	private int    lignePrc;
+	private int    cptVal;
 
 	private boolean lectureVariable ; // permet de connaitre si nous sommes dans la déclaration des variables
 	private boolean lectureConstante; // permet de connaitre si nous sommes dans la déclaration des constantes
@@ -44,13 +49,17 @@ public class Interpreteur
 	 */
 	public Interpreteur(Controleur controleur, String nomFichier)
 	{
-		this.controleur = controleur;
-		this.nomFichier = nomFichier;
+		this.controleur  = controleur;
+		this.nomFichier  = nomFichier;
 		this.numeroLigne = 0;
-		this.move = 0;
+		this.move        = 0;
+		this.lignePrc    = 0;
+		this.cptVal      = 0;
 
-		this.lstContenu      = new ArrayList<String>();
-		this.lstDonnee       = new ArrayList<Donnee>();
+		this.lstContenu      = new ArrayList<String> ();
+		this.lstDonnee       = new ArrayList<Donnee> ();
+		this.lstValeursLues  = new ArrayList<String> ();
+		this.lstBk           = new ArrayList<Integer>();
 		this.traceDexecution = "";
 
 		this.lectureVariable  = false;
@@ -86,18 +95,40 @@ public class Interpreteur
 			if ( ligneAInterpreter.equals("constante:") )this.lectureConstante = true;
 
 			if ( (this.lectureVariable || this.lectureConstante) && 
-			    !(ligneAInterpreter.equals("constante:") || ligneAInterpreter.equals("variable:")) )
+				!(ligneAInterpreter.equals("constante:") || ligneAInterpreter.equals("variable:")) )
 			{
 				// Alan c'est ton moment
 				this.creerDonnee(ligneAInterpreter);
+				
 			}
 			else
 			{
 				if ( ligneAInterpreter.contains("ecrire") )this.traceDexecution += EntreeSortie.ecrire(ligneAInterpreter, this) + "\n";
 				if ( ligneAInterpreter.contains("<--"   ) )this.affecter(ligneAInterpreter);
-				if ( ligneAInterpreter.contains("lire"  ) )EntreeSortie.lire(ligneAInterpreter, this);
+				if ( ligneAInterpreter.contains("lire"  ) )EntreeSortie.lire(ligneAInterpreter, this);		
 			}
 		}
+
+		this.lignePrc = n;
+	}
+
+	public void reset()
+	{
+		this.lstDonnee = new ArrayList<Donnee>();
+	}
+
+	public void goTo(int n)
+	{
+		if ( n < 0 || n >= this.lstContenu.size() )return;
+		
+		int courant = 1;
+		if( this.lignePrc > n )//Si on recule
+			this.reset();
+		else
+			courant = this.lignePrc;
+		
+		while(courant <= n)
+			this.interpreter(courant++);
 	}
 
 	/**
@@ -147,7 +178,7 @@ public class Interpreteur
 	 *    nombre de données
 	 */
 	public int getNbDonnee(){ return this.lstDonnee.size(); }
-
+	
 	/**
 	 * Retourne le contenu avec + ou - 40 lignes
 	 * @param n
@@ -169,55 +200,76 @@ public class Interpreteur
 		if( size <= 40)
 		{
 			for(l=0; l<size; l++)
-			{
-				//System.out.println(CouleurConsole.ROUGE.getFont());
-				if (this.numeroLigne == l)
-					res += CouleurConsole.MAUVE.getFond();
-				res+= String.format("%2d %-80s",l, this.lstContenu.get(l))+ CouleurConsole.NOIR.getFond() + "\n";
-				
-			}
-
+				res += this.getLigne(l);	
+			
 			for(int a = l; a<40; a++)
 				res += String.format("%-80s", " ") + "\n";
 		}
-		else//A debug
+		else
 		{
-			
 			int move = 0;
 			if( this.numeroLigne < 20)
 			{
-					//Affichage des 40 premieres lignes
+				//Affichage des 40 premieres lignes
 				for(i = 0+move; i<40+move; i++)
-				{
-					if (this.numeroLigne == i)
-						res += CouleurConsole.MAUVE.getFond();
-					res+= String.format("%2d %-80s",i, this.lstContenu.get(i))+ CouleurConsole.NOIR.getFond() + "\n";
-				}
+					res += this.getLigne(i);
 			}
 			else
 			{
 				move = this.numeroLigne-20;
-				for(int j = 0+move; j<size; j++)
-				{
-					if (this.numeroLigne == j)
-					{
-						res += String.format("%2d %-80s", j, CouleurConsole.MAUVE.getFond() + this.lstContenu.get(j)) + CouleurConsole.NOIR.getFond() + "\n";
-					}
-					else
-						res += String.format("%2d %-80s", j, this.lstContenu.get(j)) + "\n";
-				}
-			}
 
-			//Ajout des lignes vides en fin de programme
-			for(i=n;i<40+n && size>i ;i++)
-				if ( i < 40+n)
-					for (int j=0;j<40+n-i;j++)
-						res += String.format("%-80s", "") + "\n";
-		
+				if((size-move)>40)
+					for(int j = move; j<size; j++)
+						res += this.getLigne(j);
+				else
+					for (int j = size-40; j < size; j++)
+						res += this.getLigne(j);
+			}		
 		}
 		return res;
 	}
-	
+	/** Change le format de la ligne en paramètre
+	 * 
+	 * @param val
+	 * 	la ligne à lire
+	 * @return
+	 * 	la ligne à lire avec le bon format
+	 */
+	public String getLigne(int val)
+	{
+		String sRet = "";
+		if (this.numeroLigne == val)
+			sRet = ansi().bg(BLUE) + "";
+		sRet+= String.format("%2d %-80s",val, this.lstContenu.get(val))+ "\n";
+		return sRet;
+	}
+
+	/** Ajoute un point d'arret
+	 * @param ligne
+	 * 	Ligne du point d'arret
+	 * @return 
+	 * 	Retourne true si la ligne est ajoutée
+	 */
+	public boolean addBk(int ligne)
+	{
+		if(this.lstBk.contains(ligne)) return false;
+		this.lstBk.add(ligne);
+		return true;
+	}
+
+	/** Enleve un point d'arret
+	 * @param ligne
+	 * 	Ligne du point d'arret
+	 * @return
+	 * 	Retourne true si la ligne est enlevée
+	 */
+	public boolean rmBk(int ligne)
+	{
+		if(!this.lstBk.contains(ligne)) return false;
+		this.lstBk.remove(ligne);
+		return true;
+	}
+
 	/**
 	 * permet de creer une donnee
 	 * constante ou variable
