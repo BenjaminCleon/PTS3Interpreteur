b8ff6e6be56c97cac8bd77c5f95fc78cbcbab765
Benjihs2002 <cb201203@etu.univ-lehavre.fr>
2022-01-07 12:18:06 +0100

    Tableau

--- Metier/Donnee.java
+++ Metier/Donnee.java
@@ -1,5 +1,6 @@
  package AlgoPars.Metier;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -56,7 +57,66 @@ public class Donnee<E>
 	/**
 	 * @return la valeur de la donnée
 	 */
-	public E      getValeur() { return this.valeur; }
+	public E      getValeur(){ return this.valeur; }
+
+	/**
+	 * Si la donnée est un tableau alors cela retourne la valeur situé à cette donnée
+	 * @param args
+	 * @return
+	 */
+	public Object getValeurTableau(Integer... args)
+	{
+		if ( this.valeur instanceof List )
+		{
+			if ( ((List)this.valeur).get(0) instanceof List )
+			{
+				if ( ((List)((List)this.valeur).get(0)) instanceof List )
+					return ((List)((List)((List)this.valeur).get(args[0])).get(args[1])).get(args[2]);
+			}
+			else
+				return ((List)((List)this.valeur).get(args[0])).get(args[1]);
+		}
+		else
+			return ((List)this.valeur).get(args[0]);
+	}
+
+	/**
+	 * Permet la création de tableau jusqu'à trois dimensions
+	 * @param dimen
+	 *     nombre de dimensions du tableau
+	 * @param args
+	 *     taille de chaque dimension
+	 */
+	public void creerDimension(int dimen, Integer... args)
+	{
+		List<Object> l = ((List)this.valeur);
+
+		for ( int i=0;i<args[0];i++)
+		{
+			if ( dimen >= 2 )
+			{
+				l.add(new ArrayList<Object>());
+
+				for ( int j=0;j<args[1];j++)
+				{
+					if ( dimen == 3 )
+					{
+						((List<Object>)l.get(i)).add(new ArrayList<Object>());
+
+						for (int k=0;k<args[2];k++)
+						{
+							List<Object> tmp = ((List<Object>)l.get(i));
+							((List<Object>)tmp.get(j)).add(null);
+						}
+					}
+					else
+						((List<Object>)l.get(i)).add(null);
+				}
+			}
+			else
+				l.add(null);
+		}
+	}
 
 	/**
 	 * @return true si la donnee est constante
--- Test.java
+++ Test.java
@@ -1,8 +1,43 @@
 import static org.fusesource.jansi.Ansi.*;
 import static org.fusesource.jansi.Ansi.Color.*;
 import org.fusesource.jansi.AnsiConsole;
+import java.util.ArrayList;
+import java.util.List;
 
 class Test{
 	public static void main(String[] args) {
-		System.out.println();		
-	}}
+		List<Object> l = new ArrayList<Object>();
+		System.out.println(l);
+		Test.creerDimension(3, l, 4, 5, 2);
+		System.out.println(l);
+	}
+
+	public static void creerDimension(int dimen, List<Object> l, Integer... args)
+	{
+		for ( int i=0;i<args[0];i++)
+		{
+			if ( dimen >= 2 )
+			{
+				l.add(new ArrayList<Object>());
+
+				for ( int j=0;j<args[1];j++)
+				{
+					if ( dimen == 3 )
+					{
+						((List<Object>)l.get(i)).add(new ArrayList<Object>());
+
+						for (int k=0;k<args[2];k++)
+						{
+							List<Object> tmp = ((List<Object>)l.get(i));
+							((List<Object>)tmp.get(j)).add(null);
+						}
+					}
+					else
+						((List<Object>)l.get(i)).add(null);
+				}
+			}
+			else
+				l.add(null);
+		}
+	}
+}
