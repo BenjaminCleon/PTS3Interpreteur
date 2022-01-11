931efd8d7da16b6b1bbe84cb86dd752bbcc957e2
Benjihs2002 <cb201203@etu.univ-lehavre.fr>
2022-01-11 08:37:04 +0100

    debut primitive

--- Metier/Util.java
+++ Metier/Util.java
@@ -22,9 +22,12 @@ import iut.algo.Console;
  */
 public class Util
 {
-	private static final String REGEX_OP  = "(\\(|\\)|<=|>=|!=|\\^|<|>|=|div|mod|xou|ou|et|non|\\+|-|×|©|\\/\\^|\\/|\\\\\\/ ̄ |\\|-?\\d*\\|){1}";
+	private static final String REGEX_OP    = "(\\(|\\)|<=|>=|!=|\\^|<|>|=|div|mod|xou|ou|et|non|\\+|-|×|©|\\/\\^|\\/|\\\\\\/¯|\\|-?\\d*\\|){1}";
+	private static final String REGEX_PRIMI = "ord\\(|car\\(|enChaine\\(|enEntier\\(|enReel\\(|plancher\\(|arrondi\\(";
+	
 	private static final String TRUE  = "true" ;
 	private static final String FALSE = "false";
+	
 	/**
 	 * 
 	 * @param ligne
@@ -80,11 +83,9 @@ public class Util
 		{
 			type = interpreteur.getDonnee(valeur[0].replaceAll(" |\t", "")).getType();
 
-			if ( type.equals(Type.ENTIER) || type.equals(Type.REEL) || type.equals(Type.BOOLEEN) )
+			if ( !type.equals(Type.CHAINE) )
 				valeur[1] = String.valueOf(Util.expression(valeur[1].replaceAll(" ", ""), interpreteur));
 			
-			if ( type.equals(Type.CHAR) || type.equals(Type.BOOLEEN) )valeur[1] = valeur[1].replaceAll(" ", "");
-
 			if ( type.equals(Type.CHAINE) ) valeur[1] = Util.expression(valeur[1], interpreteur);
 		}
 
@@ -108,6 +109,11 @@ public class Util
 	   
 	public static String expression(String ligne, Interpreteur interpret)
 	{
+		return Util.expression(ligne, interpret, true);
+	}
+
+	public static String expression(String ligne, Interpreteur interpret, boolean bPrimi)
+	{
 		int taille   ;
 		int dernierOp;
 
@@ -123,6 +129,9 @@ public class Util
         Queue<String> fileRet = new LinkedList<String>();
 
 		dernierOp = 0;
+
+		if ( bPrimi ) ligne = Util.traiterPrimi(ligne, interpret);
+
 		taille = ligne.length();
 		
 		while ( (operateur = nextOperateur(ligne)) != null || ligneTmp.equals(operateur) )
@@ -132,7 +141,6 @@ public class Util
 			System.out.println("|"+ligneTmp+"|"+((dataTmp!=null)?dataTmp.getValeur():null)+"|");
 			if ( dataTmp != null )ligneTmp = String.valueOf(dataTmp.getValeur());
 			
-
 			file.add(ligneTmp);
 			
 			switch(operateur)
@@ -158,6 +166,7 @@ public class Util
 		dataTmp = interpret.getDonnee(ligne.replaceAll(" *", ""));
 		if ( dataTmp != null )ligne = String.valueOf(dataTmp.getValeur());
 		else                  ligne = ligne.replaceAll("^ *\"|\" *$", "");
+		
 		file.add(ligne);
 
         while ( !pile.isEmpty() )file.add(pile.pop());
@@ -183,11 +192,11 @@ public class Util
 			    {
 			    	lstOpeLogique.add(val);
 			    }
-			    else if(val.matches("(\\\\\\/ ̄ |\\|-?\\d*\\|){1}"))
+			    else if(val.matches("(\\\\\\/¯|\\|-?\\d*\\|){1}"))
 			    {
 			    	switch(val) //
 		        	{
-		        	   	case "\\/ ̄ " -> pile.add( String.valueOf( (Math.sqrt (Double.parseDouble(file.remove())) )) );
+		        	   	case "\\/¯" -> pile.add( String.valueOf( (Math.sqrt (Double.parseDouble(file.remove())) )) );
 		        	   	default ->
 		        	   	{
 		        	   		val=val.replace('|', ' ');
@@ -290,32 +299,77 @@ public class Util
 		Matcher matcher   = pattern.matcher(entreeLigne);
 
 		if ( matcher.find() )
-		{
-			System.out.println(matcher.start() + " " + matcher.end() );
 			return ligne.substring(matcher.start(), matcher.end());
-		}
+
 		return nextOperateur;
 	}
 
-	public Character car( int  value ){ return (char)value; }
-	public Integer   ord( char value ){ return (int )value; }
+	private static String traiterPrimi(String ligne, Interpreteur interpret)
+	{
+		System.out.println(ligne);
+		String res = "";
+		String tmp = "";
 
-	public String enChaine ( double value ){ return String.valueOf(value); }
-	public String enChaine ( int    value ){ return String.valueOf(value); }
+		String ligneNext = "";
+		String postLigne = "";
 
-	public Integer enEntier( String chaine ){ return Integer.parseInt   (chaine); }
-	public Double  enReel  ( String chaine ){ return Double .parseDouble(chaine); }
+		int posLastBracket   = 0;
+		int nbBracketWaiting = 1;
 
-	public Integer plancher ( double value ){ return (int)Math.floor(value); }
-	public Integer arrondi  ( double value ){ return (int)Math.round(value); }
+		CharSequence entreeLigne = ligne;
+		String patternStr = REGEX_PRIMI;
+		Pattern pattern = Pattern.compile(patternStr) ;
+		Matcher matcher1 = pattern.matcher(entreeLigne);
+		Matcher matcher2;
 
-	public String aujourdhui(){ return DateTimeFormatter.ofPattern("dd/MM/yyyy").format(LocalDateTime.now()); }
-	public String jour      (){ return aujourdhui().substring(0, 2); }
-	public String mois      (){ return aujourdhui().substring(3, 5); }
-	public String annee     (){ return aujourdhui().substring(6)   ; }
+		if ( !matcher1.find() )return Util.expression(ligne, interpret, false);
 
-	public Boolean estReel  ( String chaine ){ return chaine.matches("^\\d+.\\d+$"); }
-	public Boolean estEntier( String chaine ){ return chaine.matches("^\\d$"      ); }
+		for (int i=matcher1.start();i<ligne.length()&&nbBracketWaiting!=0;i++)
+		{
+			if ( ligne.charAt(i) == ')' ){ posLastBracket = i; nbBracketWaiting--; }
+			if ( ligne.charAt(i) == '(' )nbBracketWaiting++;
+		}
 
-	public Integer hasard( int valeur ){ return (int)(Math.random()*valeur); }
+		ligneNext = ligne.substring(matcher1.end(), posLastBracket);
+		/* postLigne = ligne.substring(posLastBracket+1);
+		System.out.print("[" + postLigne + "]");
+		System.out.println( "{"  + ((postLigne.equals(""))?"":Util.expression(postLigne, interpret, true)) + "}"); */
+
+		matcher2 = pattern.matcher(ligneNext);
+		if ( matcher2.find() ) tmp = Util.expression(ligneNext, interpret, true );
+		else                   tmp = Util.expression(ligneNext, interpret, false);
+
+		System.out.println(ligne.substring(matcher1.start(), matcher1.end()-1 ) + "|" +  tmp);
+
+		switch( ligne.substring(matcher1.start(), matcher1.end()-1 ) )
+		{
+			case "car" -> res = String.valueOf(Util.car(Integer.parseInt(tmp)));
+			case "ord" -> res = String.valueOf(Util.ord(tmp.charAt(1)));
+		}
+
+		System.out.println(res);
+		return res;
+	}
+
+	private static Character car( int  value ){ return (char)value; }
+	private static Integer   ord( char value ){ return (int )value; }
+
+	private static String enChaine ( double value ){ return String.valueOf(value); }
+	private static String enChaine ( int    value ){ return String.valueOf(value); }
+
+	private static Integer enEntier( String chaine ){ return Integer.parseInt   (chaine); }
+	private static Double  enReel  ( String chaine ){ return Double .parseDouble(chaine); }
+
+	private static Integer plancher ( double value ){ return (int)Math.floor(value); }
+	private static Integer arrondi  ( double value ){ return (int)Math.round(value); }
+
+	private static String aujourdhui(){ return DateTimeFormatter.ofPattern("dd/MM/yyyy").format(LocalDateTime.now()); }
+	private static String jour      (){ return aujourdhui().substring(0, 2); }
+	private static String mois      (){ return aujourdhui().substring(3, 5); }
+	private static String annee     (){ return aujourdhui().substring(6)   ; }
+
+	private static Boolean estReel  ( String chaine ){ return chaine.matches("^\\d+.\\d+$"); }
+	private static Boolean estEntier( String chaine ){ return chaine.matches("^\\d$"      ); }
+
+	private static Integer hasard( int valeur ){ return (int)(Math.random()*valeur); }
 }
